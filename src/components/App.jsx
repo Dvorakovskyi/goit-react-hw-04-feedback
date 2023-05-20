8cc22c019291ef7cd71feedfd9119fe257b3f267
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-05-20 14:06:19 +0300

    Виконав рефакторинг
    Замінив state на React-хуки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,73 +1,78 @@
 import React from 'react';
+import { useState } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from 'components/Section/Section';
 import Notification from 'components/Notification/Notification';
 import { StyledWrapper } from './App.styled';
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const statistics = {
+    good,
+    neutral,
+    bad,
   };
 
-  handleBtnClick = option => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1,
-    }));
+  const handleBtnClick = option => {
+    option === 'good' && setGood(prevState => prevState + 1);
+
+    option === 'neutral' && setNeutral(prevState => prevState + 1);
+
+    option === 'bad' && setBad(prevState => prevState + 1);
   };
 
-  countTotalFeedback = () => {
-    const values = Object.values(this.state);
+  const countTotalFeedback = () => {
+    const values = Object.values(statistics);
 
     return values.reduce((acc, value) => {
       return value + acc;
     }, 0);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    if (this.countTotalFeedback() !== 0) {
-      return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  const countPositiveFeedbackPercentage = () => {
+    if (countTotalFeedback() !== 0) {
+      return Math.round((good / countTotalFeedback()) * 100);
     } else {
       return 0;
     }
   };
 
-  render() {
-    const totalFeedback = this.countTotalFeedback();
-    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+  const totalFeedback = countTotalFeedback();
+  const positiveFeedbackPercentage = countPositiveFeedbackPercentage();
 
-    return (
-      <StyledWrapper>
+  return (
+    <StyledWrapper>
+      <Section
+        title="Please leave your feedback"
+        children={
+          <FeedbackOptions
+            options={Object.keys(statistics)}
+            onLeaveFeedback={handleBtnClick}
+          />
+        }
+      />
+      {totalFeedback <= 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
         <Section
-          title="Please leave your feedback"
+          title="Statistics"
           children={
-            <FeedbackOptions
-              options={Object.keys(this.state)}
-              onLeaveFeedback={this.handleBtnClick}
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positiveFeedback={positiveFeedbackPercentage}
             />
           }
         />
-        {totalFeedback <= 0 ? (
-          <Notification message="There is no feedback" />
-        ) : (
-          <Section
-            title="Statistics"
-            children={
-              <Statistics
-                good={this.state.good}
-                neutral={this.state.neutral}
-                bad={this.state.bad}
-                total={totalFeedback}
-                positiveFeedback={positiveFeedbackPercentage}
-              />
-            }
-          />
-        )}
-      </StyledWrapper>
-    );
-  }
-}
+      )}
+    </StyledWrapper>
+  );
+};
 
 export default App;
